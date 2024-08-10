5783e96e8b29698ae84689058f5255e716d5688b
userSarvar <itspecialist2200@gmail.com>
2024-08-10 15:46:50 +0500

    back 2 updated

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post('/submit-promoter', async (req, res) => {
 
 // Serve the promoter page at the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'promoter.html'));
+    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
 // Set the port for the server
